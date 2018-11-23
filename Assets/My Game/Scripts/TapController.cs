ed780ab2314661720a1ca252550fd4ce8ad9d9ca
sresl <sarah.resl21@gmail.com>
2018-11-23 08:26:25 +0100

    null object fixed

--- Assets/My Game/Scripts/TapController.cs
+++ Assets/My Game/Scripts/TapController.cs
@@ -4,56 +4,57 @@ using UnityEngine;
 
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class TapController : MonoBehaviour {
+public class TapController : MonoBehaviour
+{
 
     public float tapForce = 200;
     public float tiltSmooth = 2;
     public Vector3 startPos;
 
-    Rigidbody2D rigidbody;
+    Rigidbody2D rigidbodyGenji;
     Quaternion downRotation;
     Quaternion forwardRotation;
 
 
-    void start() {
+    void Start()
+    {
 
-        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbodyGenji = GetComponent<Rigidbody2D>();
         downRotation = Quaternion.Euler(0, 0, -90);
         forwardRotation = Quaternion.Euler(0, 0, 35);
         // rigidbody.simulated = false;
 
     }
 
-    void Update() {
+    void Update()
+    {
 
-        if (Input.GetMouseButtonDown(0)){
-            Debug.Log("in mouse down");
-
+        if (Input.GetMouseButtonDown(0))
+        {
             transform.rotation = forwardRotation;
-            if(rigidbody == null)
-            {
-                Debug.Log("RB null");
-            }
-            rigidbody.velocity = Vector3.zero;
-            rigidbody.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
+            rigidbodyGenji.velocity = Vector3.zero;
+            rigidbodyGenji.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
         }
 
         transform.rotation = Quaternion.Lerp(transform.rotation, downRotation, tiltSmooth * Time.deltaTime);
 
     }
 
-    void OnTriggerEnter2D(Collider2D col) {
+    void OnTriggerEnter2D(Collider2D col)
+    {
 
-        if(col.gameObject.tag == "ScoreZone") {
+        if (col.gameObject.tag == "ScoreZone")
+        {
 
             // register a score event
             // play a sound
 
         }
 
-        if(col.gameObject.tag == "DeadZone") {
+        if (col.gameObject.tag == "DeadZone")
+        {
 
-           //rigidbody.simulated = false;
+            //rigidbody.simulated = false;
 
             // register a dead event
             // play a sound
